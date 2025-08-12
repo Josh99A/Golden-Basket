57158cacd5c6e66f0684fd3bef30946c8a657e93
Awule Joshua <128831208+Josh99A@users.noreply.github.com>
2025-08-12 23:23:37 +0300

    Modified home page

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,9 @@ export default function Home() {
           <p className="mb-6">
             Whether you&apos;re a farmer, distributor, researcher, or passionate about agribusiness, we&apos;d love to hear from you! Partner with us to bring sustainable food solutions to communities and make a lasting impact.
           </p>
-          <button className="bg-gold font-semibold px-6 py-3 rounded">Contact us</button>
+          <Link href='/contact'>
+          <button className="bg-gold font-semibold px-6 py-3 rounded cursor-pointer">Contact us</button>
+          </Link>
         </div>
       </section>
       
